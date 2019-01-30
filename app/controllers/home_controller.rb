6d60fd039bb1751436d590e8fa6de071cfe33f12
Eizer G. Relayson <egrelayson@up.edu.ph>
2019-01-30 13:38:34 +0800

    Added a login system using the sessions controller. Users logged in cannot access the Login/Signup/Main Pages without logging out, and those who are not logged in should only be able to access the placeholder for the Group Sessions Page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :save_login_state, :only => [:index] #Prevent access to the home page if user is already logged in
+  
   def index
   end
 end
