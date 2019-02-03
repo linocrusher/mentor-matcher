1b7b3e9cdfcccd167ec917b28aa662cc04fa2abd
Eizer G. Relayson <egrelayson@up.edu.ph>
2019-02-03 20:01:45 +0800

    Can now add group sessions. Has a bug where group session creation returns a weird page but refreshing it should do the trick. Added validations to the group session model and a list for the group session index page.

--- app/controllers/group_sessions_controller.rb
+++ app/controllers/group_sessions_controller.rb
@@ -6,23 +6,20 @@ class GroupSessionsController < ApplicationController
 
 	def create
   	  	@group_session = GroupSession.new(group_session_params)
-  	  	if type == "PM"
-  	  		@group_session.hour = @group_session.hour + 12
-  	  	end
-  	  	@group_session.schedule = DateTime.new(:date.year, :date.month, :date.day, :hour, :minute, 0)
   		if @group_session.save
   			render 'index'
+  			redirect_to group_session_path
   		else
   			render 'new'
   		end
 	end
 
 	def index
-		@current
+		@group_sessions = GroupSession.all
 	end
 
 	private
 	def group_session_params #For strong parameters
-		params.require(:group_session).permit(:subject, :date, :venue, :topic, :hour, :minute, :type)
+		params.require(:group_session).permit(:subject, :schedule, :venue, :topic)
 	end
 end
--- app/models/group_session.rb
+++ app/models/group_session.rb
@@ -1,2 +1,6 @@
 class GroupSession < ApplicationRecord
+	validates :subject, presence: true
+	validates :schedule, presence: true
+	validates :venue, presence: true
+	validates :topic, presence: true
 end
